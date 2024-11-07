967b63f9b30ff2275c6f91275152efb08061e603
jonathanrbt <jonathanstiwarromerobetancourt@gmail.com>
2024-11-07 04:22:52 -0500

    feat: :sparkles: se crea funciones para leer y escribir archivos json

---
+++ modulos_acme/archivos_acme.py
@@ -0,0 +1,16 @@
+import json
+
+
+def leer_json(archivo):
+    try:
+        with open('archivos/archivo_libros.json', 'r') as file_libros:
+            return json.load(file_libros)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+
+
+
+def escribir_json(datos, archivo):
+    with open(archivo, 'w') as file_libros:
+        json.dump(datos, file_libros, indent=4)
