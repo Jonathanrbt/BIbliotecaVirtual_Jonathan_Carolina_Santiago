6acb755fd4b28fb7227a6193af7c5d12b3cbbdf2
Santy <santy.acevedo2005@gmail.com>
2024-11-07 04:32:54 -0500

    feat: se añanden a lista de diccionarios

--- modulos_acme/funciones_acme.py
+++ modulos_acme/funciones_acme.py
@@ -1,3 +1,18 @@
 import json
 
 def reporte():
+    report = leer_json
+
+    codigo = report["codigo"]
+    titulo_lib = report["titulo"]
+    sinopsis = report["sinopsis"]
+    autor = report["autor"]
+    fecha_publ = report["fecha"]
+    cantidades = report["cantidad"]
+
+    for codigo in report.items():
+        report.append({
+            "Codigo": codigo,
+
+
+        }) 
